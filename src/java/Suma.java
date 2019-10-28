0ff2da49b25afa5efc191d691e1ff5dbd51bc57e
daw201 <daw201@daw201-HP-15-Notebook-PC>
2019-10-28 08:48:22 +0100

    Modificaciones con la suma realizada

---
+++ src/java/Suma.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author daw201
+ */
+public class Suma {
+    private int num1;
+    private int num2;
+    
+    private int resultado;
+
+    public Suma() {
+    }
+
+    public Suma(int num1, int num2, int resultado) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.resultado = resultado;
+    }
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    public int getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(int resultado) {
+        this.resultado = resultado;
+    }
+
+    @Override
+    public String toString() {
+        return "Suma{" + "num1=" + num1 + ", num2=" + num2 + ", resultado=" + resultado + '}';
+    }
+}
---
+++ web/suma.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : suma
+    Created on : 28-oct-2019, 8:45:24
+    Author     : daw201
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Suma</title>
+    </head>
+    <body>
+        <h1>Suma</h1>
+    </body>
+</html>
